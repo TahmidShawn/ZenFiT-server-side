0012f196b87e6351f17c5772c341f02af3a38ae0
TahmidShawn7 <tahmidshawn7@gmail.com>
2023-11-26 13:00:51 +0600

    added trainer/id route to get single trainer data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,13 @@ async function run() {
   // get trainer data
   app.get("/trainer", async (req, res) => {
     const result = await trainerCollection.find().toArray();
+    res.send(result);
+  });
+  // get trainer data by id
+  app.get("/trainer/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }; 
+    const result = await trainerCollection.findOne(query);
     res.send(result);
   });
 
